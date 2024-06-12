a13dae37b5460bdc35a248bf719d9ae9c28ee748
bhsunilvarma13 <mailme4loops@gmail.com>
2024-06-12 06:59:54 +0530

    feat: Add endpoints for changing password, getting current user, updating account, and updating avatar and cover image

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateAccount,
+  updateUserAvatar,
+  updateUserCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -29,5 +36,33 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 
 router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+
+router.route("/update-account").patch(verifyJWT, updateAccount);
+
+router.route("/update-avatar").patch(
+  verifyJWT,
+  upload.single({
+    name: "avatar",
+    maxCount: 1,
+  }),
+  updateUserAvatar
+);
+
+router.route("/update-cover-image").patch(
+  verifyJWT,
+  upload.single({
+    name: "coverImage",
+    maxCount: 1,
+  }),
+  updateUserCoverImage
+);
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
